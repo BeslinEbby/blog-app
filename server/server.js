53600158e5d392415a00d37f1ef4edbf9e5531f9
Beslin Ebby <beslinebby00@gmail.com>
2025-12-31 21:39:58 +0530

    feat: add auth register routes

--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRouter = require("./routes/authRoutes");
 
 const app = express();
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/auth", userRouter);
 
 app.get("/", (req, res) => {
    res.send("<h1>Server Running Successfully</h1>"); 
