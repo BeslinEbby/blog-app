57796a11abcf1bee04d9eb096295d865481d1e1c
Beslin Ebby <beslinebby00@gmail.com>
2025-12-31 19:19:38 +0530

    feat: add required dependencies and setup server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config()
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+   res.send("<h1>Server Running Successfully</h1>"); 
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+   console.log(`server started on http://localhost:${port}`);
+});
