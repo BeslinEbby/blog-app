57d30ee9178d3583264959afcdb7b0fa84f7994c
Beslin Ebby <beslinebby00@gmail.com>
2026-01-01 17:48:49 +0530

    feat: add login function and route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,7 @@
 const userModel = require("../models/authModel");
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
+
 
 const registerUser = async (req, res) => {
    const { name, email, password } = req.body;
@@ -11,7 +14,7 @@ const registerUser = async (req, res) => {
       const existingUser = await userModel.findOne({ email });
 
       if (existingUser) {
-         return res.status(400).json({ success: false, message: "user already exists" });
+         return res.status(400).json({ success: false, message: "User already exists" });
       }
 
       const salt = 10;
@@ -25,7 +28,7 @@ const registerUser = async (req, res) => {
 
       const user = await newUser.save();
       if (user) {
-         res.status(201).json({ success: true, message: "user registered successfully" });
+         res.status(201).json({ success: true, message: "User registered successfully" });
       }
    } catch (error) {
       res.status(500).json({ success: false, message: "Server error", error: error.message });
@@ -33,4 +36,41 @@ const registerUser = async (req, res) => {
    }
 };
 
-module.exports=registerUser
+const loginUser = async (req, res) => {
+   const { email, password } = req.body;
+
+   try {
+      const user = await userModel.findOne({ email });
+      
+      if (!user) {
+         return res.status(400).json({ success: false, message: "User doesn't exists" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+         return res.status(400).json({sucees: false, message: "Invalid credentials" });
+      }
+      const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET, {
+         expiresIn: "24h",
+      });   
+
+      res.status(200).json({
+         success: true,
+         message: "logged in successfully",
+         token,
+         user: {
+            _id: user._id,
+            email: user.email,
+            token
+         },
+      });
+   } catch (error) {
+      res.status(500).json({ success: false, message: "Server error", error: error.message });
+      console.log("error on login user : ", error);
+   }
+};
+
+module.exports={
+   registerUser,
+   loginUser
+}
