846efef67a74f76a0b741942219a12bca589dec7
Beslin Ebby <beslinebby00@gmail.com>
2026-01-02 19:37:35 +0530

    fix: correct url for update and delete post

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -6,8 +6,8 @@ const upload = require("../middlewares/multer")
 const postRouter=express.Router()
 
 postRouter.post("/create", protect, upload.single("coverImage"), createPost)
-postRouter.put("/update", protect, upload.single("coverImage"), updatePost)
-postRouter.delete("/delete", protect, deletePost)
+postRouter.put("/update/:postId", protect, upload.single("coverImage"), updatePost)
+postRouter.delete("/delete/:postId", protect, deletePost)
 postRouter.get("/all", getAllPosts)
 postRouter.get("/user", getUserPosts)
 
