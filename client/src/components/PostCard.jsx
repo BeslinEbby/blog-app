74e3898744901190d889b79144509b39118d326c
Beslin Ebby <beslinebby00@gmail.com>
2026-01-05 17:59:36 +0530

    feat: add post card

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { MdImageNotSupported } from "react-icons/md";
+
+const PostCard = ({ post }) => {
+   const { user } = useContext(AuthContext);
+
+   return (
+      <Link to={`/post/${post._id}`} key={post._id} className="text-blue-500">
+         <div className="border-2 border-black/60  px-4 py-2 rounded-lg flex flex-col justify-between gap-2">
+            <h3 className="text-lg font-semibold capitalize">{post.title}</h3>
+            {post.coverImage ? (
+               <img src={post.coverImage} alt="" className="h-40 w-full object-fill mb-2 rounded-lg" />
+            ) : (
+               <div className="w-full h-40 text-gray-500 bg-gray-300 rounded-lg flex items-center justify-center mb-2">
+                  <MdImageNotSupported className=" w-full h-1/3" />
+               </div>
+            )}
+            <p className="text-sm line-clamp-1">{post.content.slice(0, 60)}...</p>
+            <span className="font-semibold">By {post.author?._id === user._id ? "You" : post.author?.name}</span>
+         </div>
+      </Link>
+   );
+};
+
+export default PostCard;
